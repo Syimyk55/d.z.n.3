36db99e51a78d384075dbedebbf11d5e7cb1e56b
Syimyk55 <114605451+Syimyk55@users.noreply.github.com>
2022-10-09 09:26:23 +0600

    homework3

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,21 @@
 class average {
     public static void main(String[] args) {
-        double[] numbers = new double[15];
-        numbers[0] = 1.2;
-        numbers[1] = -2.4;
-        numbers[2] = 6.2;
-        numbers[3] = -8.9;
-        numbers[4] = 8.2;
-        numbers[5] = -2.6;
-        numbers[6] = 2.9;
-        numbers[7] = -7.5;
-        numbers[8] = 5.5;
-        numbers[9] = -3.9;
-        numbers[10] = 0.3;
-        numbers[11] = -1.7;
-        numbers[12] = 0.8;
-        numbers[14] = 9.3;
-        double sum =0;
-        System.out.println(numbers[2] + numbers[4] + numbers[6] + numbers[8] + numbers[10] + numbers[12] + numbers[14]);
-        for (double num : numbers){
-           sum +=num;
-        }
-        System.out.println("реднее арифметическое число равно  "   + sum/ numbers.length);
+        double numbers[] = {-3.5, 4.5,-7.3, 9.3,-3.1, 8.3,
+                9.6,-8.3, 9.9, 1.2,-6.7, 0.4, 0.9, 1.2, 4.1};
+        double sam = 0;
+        int f =0;
+       boolean c = false;
+for (double j:numbers){
+    if (j < 0){
+        c = true;
     }
+    if (j > 0 && c ){
+      sam = sam + j;
+      f++;
+    }
+}
+        System.out.println(sam/f);
+
+    }
+
 }
